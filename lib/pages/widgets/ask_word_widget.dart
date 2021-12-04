5b4a0ba3a3a785a7a86ba03df4c2fa85db0706b9
Jiman Hwang <40803236+molehair@users.noreply.github.com>
2021-12-04 18:07:37 +0900

    added example widget

--- lib/pages/widgets/ask_word_widget.dart
+++ lib/pages/widgets/ask_word_widget.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:just_the_tooltip/just_the_tooltip.dart';
 
 enum _Status {
   waiting,
@@ -30,6 +29,8 @@ class _AskWordWidgetState extends State<AskWordWidget>
     with AutomaticKeepAliveClientMixin {
   _Status _status = _Status.waiting;
   Word? _choice;
+  bool _viewMnemonic = false;
+  bool _viewDef = false;
 
   void _changeStatus(_Status newStatus) {
     setState(() {
@@ -72,82 +73,62 @@ class _AskWordWidgetState extends State<AskWordWidget>
           ),
           Visibility(
             visible: _status == _Status.waiting,
-            child: Row(
-              children: [
-                Flexible(
-                  child: TypeAheadField(
-                    textFieldConfiguration: TextFieldConfiguration(
-                      enabled: _status == _Status.waiting,
-                      // autofocus: true,
-                      style: DefaultTextStyle.of(context)
-                          .style
-                          .copyWith(fontStyle: FontStyle.italic),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              // color: Colors.red,
-                              // color: Theme.of(context).errorColor,
-                              ),
+            child: TypeAheadField(
+              textFieldConfiguration: TextFieldConfiguration(
+                enabled: _status == _Status.waiting,
+                // autofocus: true,
+                style: DefaultTextStyle.of(context)
+                    .style
+                    .copyWith(fontStyle: FontStyle.italic),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        // color: Colors.red,
+                        // color: Theme.of(context).errorColor,
                         ),
-                        prefixIcon: Icon(Icons.short_text_outlined),
-                      ),
-                      // controller: _answerController,
-                    ),
-                    suggestionsCallback: (pattern) async {
-                      // search definitions from the index
-                      final Set<int> suggestionKeys =
-                          Dictionary.nameIndex.search(pattern);
-
-                      // fetch definitions
-                      final List<Word> words = [];
-                      final Box<Word> box =
-                          await Hive.openBox(Dictionary.boxName);
-                      for (int key in suggestionKeys) {
-                        Word? word = box.get(key);
-                        if (word != null) {
-                          words.add(word);
-                        }
-                      }
-
-                      return words;
-                    },
-                    itemBuilder: (context, Word suggestion) {
-                      return ListTile(
-                        // leading: Icon(Icons.shopping_cart),
-                        title: Text(suggestion.name),
-                        // subtitle: Text('\$${suggestion['price']}'),
-                      );
-                    },
-                    onSuggestionSelected: _grade,
-                    loadingBuilder: (context) => ListTile(
-                      title: Text(AppLocalizations.of(context)!.loading),
-                    ),
-                    noItemsFoundBuilder: (context) => ListTile(
-                      title: Text(
-                        AppLocalizations.of(context)!.notFound,
-                        style: TextStyle(color: Theme.of(context).errorColor),
-                      ),
-                    ),
-                    // errorBuilder: (BuildContext context, Object? error) => Text(
-                    //   '$error AAA',
-                    //   style: TextStyle(color: Theme.of(context).errorColor),
-                    // ),
                   ),
+                  prefixIcon: Icon(Icons.short_text_outlined),
                 ),
-                Visibility(
-                  visible: widget.question.word.mnemonic != null &&
-                      widget.question.word.mnemonic != '',
-                  child: JustTheTooltip(
-                    child: const Icon(Icons.lightbulb_outline),
-                    content: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(widget.question.word.mnemonic ?? ''),
-                    ),
-                    triggerMode: TooltipTriggerMode.tap,
-                    preferredDirection: AxisDirection.up,
-                  ),
+                // controller: _answerController,
+              ),
+              suggestionsCallback: (pattern) async {
+                // search definitions from the index
+                final Set<int> suggestionKeys =
+                    Dictionary.nameIndex.search(pattern);
+
+                // fetch definitions
+                final List<Word> words = [];
+                final Box<Word> box = await Hive.openBox(Dictionary.boxName);
+                for (int key in suggestionKeys) {
+                  Word? word = box.get(key);
+                  if (word != null) {
+                    words.add(word);
+                  }
+                }
+
+                return words;
+              },
+              itemBuilder: (context, Word suggestion) {
+                return ListTile(
+                  // leading: Icon(Icons.shopping_cart),
+                  title: Text(suggestion.name),
+                  // subtitle: Text('\$${suggestion['price']}'),
+                );
+              },
+              onSuggestionSelected: _grade,
+              loadingBuilder: (context) => ListTile(
+                title: Text(AppLocalizations.of(context)!.loading),
+              ),
+              noItemsFoundBuilder: (context) => ListTile(
+                title: Text(
+                  AppLocalizations.of(context)!.notFound,
+                  style: TextStyle(color: Theme.of(context).errorColor),
                 ),
-              ],
+              ),
+              // errorBuilder: (BuildContext context, Object? error) => Text(
+              //   '$error AAA',
+              //   style: TextStyle(color: Theme.of(context).errorColor),
+              // ),
             ),
           ),
           Visibility(
@@ -180,29 +161,39 @@ class _AskWordWidgetState extends State<AskWordWidget>
               ),
             ),
           ),
-          Visibility(
-            visible: _status != _Status.waiting,
-            child: Card(
-              child: ListTile(
-                leading: const Icon(Icons.short_text_outlined),
-                title: Text(
-                  widget.question.word.def,
-                  // style: TextStyle(fontWeight: FontWeight.bold),
-                ),
+          Card(
+            child: ListTile(
+              leading: const Icon(Icons.short_text_outlined),
+              title: Text(
+                _viewDef
+                    ? widget.question.word.ex
+                    : AppLocalizations.of(context)!.viewDefinition,
+                style: _viewDef
+                    ? null
+                    : const TextStyle(fontStyle: FontStyle.italic),
               ),
+              onTap: () => setState(() {
+                _viewDef = !_viewDef;
+              }),
             ),
           ),
           Visibility(
-            visible: _status != _Status.waiting &&
-                widget.question.word.mnemonic != null &&
+            visible: widget.question.word.mnemonic != null &&
                 widget.question.word.mnemonic != '',
             child: Card(
               child: ListTile(
                 leading: const Icon(Icons.lightbulb_outline),
                 title: Text(
-                  widget.question.word.mnemonic ?? '',
-                  // style: TextStyle(fontWeight: FontWeight.bold),
+                  _viewMnemonic
+                      ? widget.question.word.mnemonic ?? ''
+                      : AppLocalizations.of(context)!.viewMnemonic,
+                  style: _viewMnemonic
+                      ? null
+                      : const TextStyle(fontStyle: FontStyle.italic),
                 ),
+                onTap: () => setState(() {
+                  _viewMnemonic = !_viewMnemonic;
+                }),
               ),
             ),
           ),
