e7074e8078a359628fd0ad99f60c7be565b21316
Jiman Hwang <40803236+molehair@users.noreply.github.com>
2021-12-05 12:35:17 +0900

    added searching and sorting features

--- lib/pages/dictionary_page.dart
+++ lib/pages/dictionary_page.dart
@@ -42,85 +42,135 @@ class _DictionaryPageMain extends StatefulWidget {
   __DictionaryPageMainState createState() => __DictionaryPageMainState();
 }
 
-class __DictionaryPageMainState extends State<_DictionaryPageMain> {
-  final List<Word> dictionary = [];
+class __DictionaryPageMainState extends State<_DictionaryPageMain>
+    with AutomaticKeepAliveClientMixin {
+  bool _searchMode = false;
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return ValueListenableBuilder(
       valueListenable: widget.box.listenable(),
       builder: (context, Box<Word> box, widget) {
-        // clean up the previous list
-        dictionary.clear();
+        List<Word> words = [];
 
-        // add the current ones
-        for (int key in box.keys) {
+        // prepare list to show depending on the search mode
+        Iterable<dynamic> keys = _searchMode
+            ? Dictionary.nameIndex.search(_searchController.text.trim())
+            : box.keys;
+
+        // fetch word info
+        for (int key in keys) {
           Word word = box.get(key)!;
           word.key = key;
-          dictionary.add(word);
+          words.add(word);
         }
-        return Scaffold(
-          appBar: AppBar(
-              title: Text(AppLocalizations.of(context)!.dictionary),
-              actions: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(right: 20.0),
-                  child: GestureDetector(
-                    onTap: () {},
-                    child: const Icon(
+
+        // sort
+        words.sort((Word word1, Word word2) =>
+            word1.name.toLowerCase().compareTo(word2.name.toLowerCase()));
+
+        return WillPopScope(
+          onWillPop: () {
+            if (_searchMode) {
+              // if in search mode, go back to non-search mode, not exit app
+              setState(() {
+                _searchMode = false;
+              });
+              return Future.value(false);
+            } else {
+              // not in search mode: normal exit
+              return Future.value(true);
+            }
+          },
+          child: Scaffold(
+            appBar: AppBar(
+                leading: _searchMode
+                    ? IconButton(
+                        icon: const Icon(Icons.arrow_back_ios_new_outlined),
+                        onPressed: () => setState(() {
+                          _searchMode = false;
+                        }),
+                      )
+                    : null,
+                title: _searchMode
+                    ? TextField(
+                        controller: _searchController,
+                        autofocus: true,
+                        onChanged: (_) => setState(() {}),
+                        // decoration: InputDecoration(
+                        //   fillColor: Colors.white,
+                        //   // fillColor: Theme.of(context).backgroundColor,
+                        // ),
+                        cursorColor: Colors.white,
+                        style: const TextStyle(color: Colors.white))
+                    : Text(AppLocalizations.of(context)!.dictionary),
+                actions: <Widget>[
+                  IconButton(
+                    icon: const Icon(
                       Icons.search,
                       size: 26.0,
                     ),
+                    onPressed: () {
+                      setState(() {
+                        _searchMode = true;
+                      });
+                    },
                   ),
-                ),
-              ]),
-          body: ListView.builder(
-            itemCount: dictionary.length + 1,
-            itemBuilder: (context, index) {
-              Widget widget;
-              if (index == 0) {
-                //-- header --//
-                widget = ListTile(
-                  title: Text(
-                    'Total ${dictionary.length} word' +
-                        (dictionary.length > 1 ? 's' : ''),
-                    textAlign: TextAlign.center,
-                  ),
-                  dense: true,
-                );
-              } else {
-                //-- data --//
-                index--;
-                Word word = dictionary[index];
-                widget = Card(
-                  child: ListTile(
-                    title: Text(word.name),
-                    subtitle: Text(word.ex),
-                    onTap: () => Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => WordPage(
-                          createMode: false,
-                          storeKey: word.key,
-                          word: word,
+                ]),
+            body: ListView.builder(
+              itemCount: words.length + 1,
+              itemBuilder: (context, index) {
+                Widget widget;
+                if (index == 0) {
+                  //-- header --//
+                  widget = ListTile(
+                    title: Text(
+                      AppLocalizations.of(context)!
+                          .totalNumWords
+                          .replaceFirst('\$', words.length.toString()),
+                      textAlign: TextAlign.center,
+                    ),
+                    dense: true,
+                  );
+                } else {
+                  //-- data --//
+                  index--;
+                  Word word = words[index];
+                  widget = Card(
+                    child: ListTile(
+                      title: Text(word.name),
+                      subtitle: Text(word.ex),
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => WordPage(
+                            createMode: false,
+                            storeKey: word.key,
+                            word: word,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                );
-              }
-              return widget;
-            },
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => WordPage(createMode: true),
-              ),
+                  );
+                }
+                return widget;
+              },
             ),
-            child: const Icon(Icons.add),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () => Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => WordPage(createMode: true),
+                ),
+              ),
+              child: const Icon(Icons.add),
+            ),
           ),
         );
       },
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
