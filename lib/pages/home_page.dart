e4bdf6c19ed075729c9ff1d65f938d943a22ef56
molehair <40803236+molehair@users.noreply.github.com>
2022-03-27 12:51:29 +0900

    update due to database change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,13 @@
 // Home provides menus when the user signed in.
 
+import 'package:annoyer/database/practice_instance.dart';
+import 'package:annoyer/database/test_instance.dart';
+import 'package:annoyer/i18n/strings.g.dart';
+import 'package:annoyer/notification_center.dart';
+import 'package:annoyer/pages/training_page.dart';
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 import 'dictionary_page.dart';
 import 'settings_page.dart';
@@ -14,18 +20,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomeState extends State<HomePage> {
+  bool _showTrainingBadge = false;
+
+  @override
+  void initState() {
+    // add listener for training badge
+    PracticeInstance.getStream().listen(_onInstanceUpdate);
+    TestInstance.getStream().listen(_onInstanceUpdate);
+
+    // set badge status
+    _updateBadge();
+
+    // launch practice / test page if launched by tapping a notification
+    WidgetsBinding.instance!.addPostFrameCallback((_) async {
+      NotificationAppLaunchDetails? nald = await NotificationCenter
+          .flutterLocalNotificationsPlugin
+          .getNotificationAppLaunchDetails();
+      if (nald != null && nald.didNotificationLaunchApp) {
+        //-- launched by tapping --//
+        onTapNotification(nald.payload);
+      }
+    });
+
+    super.initState();
+  }
+
+  void _onInstanceUpdate(_) {
+    _updateBadge();
+  }
+
+  void _updateBadge() async {
+    // must be mounted
+    if (!mounted) {
+      return;
+    }
+
+    int pracInstCount = await PracticeInstance.count();
+    int testInstCount = await TestInstance.count();
+
+    // update state
+    setState(() {
+      _showTrainingBadge = (pracInstCount + testInstCount > 0);
+    });
+  }
+
   // page setup
   int _index = 0;
   static final List<Widget> _bodies = <Widget>[
     const DictionaryPage(),
+    const TrainingPage(),
     const SettingsPage(),
   ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _index = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,16 +81,26 @@ class _HomeState extends State<HomePage> {
       bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: const Icon(Icons.list),
-            label: AppLocalizations.of(context)!.settings,
+            icon: const Icon(Icons.menu_book_outlined),
+            label: t.dictionary,
           ),
           BottomNavigationBarItem(
-            icon: const Icon(Icons.settings),
-            label: AppLocalizations.of(context)!.dictionary,
+            icon: Badge(
+              showBadge: _showTrainingBadge,
+              badgeContent: null,
+              child: const Icon(Icons.school_outlined),
+            ),
+            label: t.training,
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.settings_outlined),
+            label: t.settings,
           ),
         ],
         currentIndex: _index,
-        onTap: _onItemTapped,
+        onTap: (index) => setState(() {
+          _index = index;
+        }),
       ),
     );
   }
