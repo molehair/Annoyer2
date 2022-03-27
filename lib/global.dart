147d1003036a7fe1db93a26b66c000dfaddde9ed
molehair <40803236+molehair@users.noreply.github.com>
2022-03-27 12:42:15 +0900

    replaced showing message dialog with snackbar

--- lib/global.dart
+++ lib/global.dart
@@ -45,20 +45,23 @@ class Global {
 
   /// show failure
   static void showFailure() {
-    showDialog(
-        context: navigatorKey.currentContext!,
-        builder: (BuildContext context) {
-          return const SimpleDialog(
-            // title: Text(title),
-            children: <Widget>[
-              Icon(
-                Icons.clear,
-                size: 50,
-                color: Colors.red,
-              ),
-            ],
-          );
-        });
+    BuildContext context = navigatorKey.currentContext!;
+    double height = MediaQuery.of(context).size.height;
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Icon(
+          Icons.clear,
+          size: 32,
+          color: Colors.white,
+        ),
+        margin: EdgeInsets.only(bottom: height / 3),
+        duration: const Duration(milliseconds: 700),
+        behavior: SnackBarBehavior.floating,
+        shape: const CircleBorder(side: BorderSide.none),
+        backgroundColor: Colors.red,
+        // elevation: 100,
+      ),
+    );
   }
 
   /// remove non-word characters(,!? and so on)
