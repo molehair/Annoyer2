db06441ad57f121e2b59a3c0070405594d3d60db
Jiman Hwang <40803236+molehair@users.noreply.github.com>
2022-06-01 16:50:05 +0900

    set the log filter for release mode

--- lib/log.dart
+++ lib/log.dart
@@ -2,12 +2,30 @@
 
 import 'package:annoyer/database/database.dart';
 import 'package:annoyer/database/log_item.dart';
+import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
+
+var _consoleLogOutput = ConsoleOutput();
+var _dbLogOutput = _DBLog();
+var _releaseModeFilter = _ReleaseModeFilter();
 
 Logger logger = Logger(
   printer: SimplePrinter(printTime: true),
-  output: MultiOutput([ConsoleOutput(), _DBLog()]),
+  output: kDebugMode
+      ? MultiOutput([_consoleLogOutput, _dbLogOutput])
+      : _dbLogOutput,
+  filter: kDebugMode ? null : _releaseModeFilter,
 );
+
+class _ReleaseModeFilter extends LogFilter {
+  @override
+  bool shouldLog(LogEvent event) {
+    return event.level == Level.info ||
+        event.level == Level.error ||
+        event.level == Level.warning ||
+        event.level == Level.wtf;
+  }
+}
 
 class _DBLog extends LogOutput {
   @override
